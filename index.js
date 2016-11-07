7235b8254c01ca79d33486b429642e54228be841
Dan Jenner <danjenner@gmail.com>
2016-11-07 16:12:12 -0600

    Cont working on UI functionality, added API queries to Yummly, displaying data, user logins/hashing and authentication working.

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ var bcrypt = require('bcryptjs');
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 var server = http.Server(app);
 var User = require('./models/user');
@@ -36,67 +34,95 @@ process.on('SIGINT', function() { //When you use control in gitbash, this event 
 
 function initApp() {
 
-      //get and app stuff here
-      //GET, displays a list of all the items in DB
-      var schema = 'User';
+    //get and app stuff here
+    //GET, displays a list of all the items in DB
+    var schema = 'User';
 
-      app.get('/user', function(req, res) {
-          User.find(function(err, items) {
-              if (err) {
-                  return res.status(500).json({
-                      message: 'Internal Server Error'
-                  });
-              } else {
-                  console.log("Got a request to disp user db data")
-                  res.json(items);
-              }
+    //Show list of all users in DB
+    app.get('/user', function(req, res) {
+        User.find(function(err, items) {
+            if (err) {
+                return res.status(500).json({message: 'Internal Server Error'});
+            } else {
+                console.log("Got a request to disp user db data")
+                res.json(items);
+            }
 
-          });
-      });
+        });
+    });
 
-      //--Creates new user in DB from login/signup main page
-        app.post('/users/create', function(req, res) {
-            console.log("Request to create: " + username, password);
-            console.log(req.body);
-            var username = req.body.username;
-            username = username.trim();
-            var password = req.body.password;
-            password = password.trim();
+    //--Login of single user from login page, protected by bcrypt and hashed PWD's
+    app.post('/login', function(req, res) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        var uname = req.body.username;
+        var pwd = req.body.password;
 
-
-            bcrypt.genSalt(10, function(err, salt) {
-                if (err) {
-                    return res.status(500).json({
-                        message: 'Internal server error'
-                    });
-                }
-
-                bcrypt.hash(password, salt, function(err, hash) {
+        User.findOne({
+            username: uname
+        }, function(err, items) {
+            if (err) {
+                return res.status(500).json({message: 'Internal Server Error'});
+            }
+            if (!items) {
+                //bad username
+                return res.status(401).json({message: 'Not found'});
+            } else {
+                items.validatePassword(pwd, function(err, isValid) {
                     if (err) {
-                        return res.status(500).json({
-                            message: 'Internal server error'
-                        });
+                        console.log("No clue what this does - if it ever logs, check it out");
                     }
+                    if (!isValid) {
+                        return res.status(401).json({message: 'Not found'});
+                    } else {
+                        console.log("User: " + uname + " logged in.")
+                        return res.json(items);
+                    }
+                    //return something here
+                });
+            }
+        });
+    });
+
+    //--Creates new user in DB from login/signup main page
+    app.post('/users/create', function(req, res) {
+        console.log("Request to create: " + username, password, email);
+        console.log(req.body);
+        var username = req.body.username;
+        username = username.trim();
+        var password = req.body.password;
+        password = password.trim();
+        var email = req.body.email;
+        email = email.trim();
+
+        bcrypt.genSalt(10, function(err, salt) {
+            if (err) {
+                return res.status(500).json({message: 'Internal server error'});
+            }
+
+            bcrypt.hash(password, salt, function(err, hash) {
+                if (err) {
+                    return res.status(500).json({message: 'Internal server error'});
+                }
 
                 User.create({
                     username: username,
                     password: hash,
+                    email: email
                 }, function(err, item) {
                     if (err) {
-                        return res.status(500).json({
-                            message: 'Couldnt create user, already exists'
-                        });
+                        return res.status(500).json({message: 'Couldnt create user, already exists'});
                     }
-                    if(item) {
-                        console.log("User: " + username + " created.");
+                    if (item) {
+                        console.log("User: " + username + " created with hash and email" + email);
                         return res.json(item);
                     }
                 });
-                });
             });
         });
+    });
 
-      server.listen(3000, function() {
+    server.listen(3000, function() {
         console.log('Server started, listening on *:3000');
     });
 
--- public/app.js
+++ public/app.js
@@ -12,77 +12,219 @@ as the name, content, recipe id number, url for image, etc
 */
 
 //Define day vars
-let monday, tuesday, wednesday, thursday, friday, weekend;
+let monday,
+    tuesday,
+    wednesday,
+    thursday,
+    friday,
+    weekend;
 
 //Demo data object
 let demoData = {
-  username: 'dan',
-  password: 'test',
-  email: 'dan@email.com',
-  menu : [
-    monday = {
-      breakfast : ['cereal', 12345],
-      lunch: ['sandwich', 125412312],
-      dinner: ['spaghetti',23423]
-    },
-    tuesday = {
-      breakfast: 'cereal',
-      lunch: 'sandwich',
-      dinner: 'chicken'
-    },
-    wednesday = {
-      breakfast: 'cereal',
-      lunch: 'sandwich',
-      dinner: 'chicken'
-    },
-    thursday = {
-      breakfast: 'cereal',
-      lunch: 'sandwich',
-      dinner: 'chicken'
-    },
-    friday = {
-      breakfast: 'cereal',
-      lunch: 'sandwich',
-      dinner: 'chicken'
-    },
-    weekend = {
-      breakfast: 'cereal',
-      lunch: 'sandwich',
-      dinner: 'chicken'
-    },
-  ]
+    username: 'dan',
+    password: 'test',
+    email: 'dan@email.com',
+    menu: [
+        monday = {
+            breakfast: [
+                'cereal', 12345
+            ],
+            lunch: [
+                'sandwich', 125412312
+            ],
+            dinner: ['spaghetti', 23423]
+        },
+        tuesday = {
+            breakfast: 'cereal',
+            lunch: 'sandwich',
+            dinner: 'chicken'
+        },
+        wednesday = {
+            breakfast: 'cereal',
+            lunch: 'sandwich',
+            dinner: 'chicken'
+        },
+        thursday = {
+            breakfast: 'cereal',
+            lunch: 'sandwich',
+            dinner: 'chicken'
+        },
+        friday = {
+            breakfast: 'cereal',
+            lunch: 'sandwich',
+            dinner: 'chicken'
+        },
+        weekend = {
+            breakfast: 'cereal',
+            lunch: 'sandwich',
+            dinner: 'chicken'
+        }
+    ]
 };
 
-console.log(demoData);
-console.log(monday.breakfast[0]);
+//console.log(demoData);
+//console.log(monday.breakfast[0]);
+
+//Functions
+
+//receives array of events, pushes specific data into event_arr array of objects for use in children component
+function dispSearch (result, index, array) {
+
+      let id = result.id;
+      let recipe_name = result.recipeName = result.recipeName ? result.recipeName : 'Name';
+      let rating = result.rating;
+      let img = result.imageUrlsBySize[90];
+   console.log(id, recipe_name, rating, img);
+
+   $('div.column_results').append("<div class='portlet ui-widget ui-widget-content ui-helper-clearfix ui-corner-all' id=" + id + "><div class='portlet-header ui-widget-header ui-corner-all'><span class='ui-icon ui-icon-minusthick portlet-toggle'></span>" + recipe_name + "</div><div class='portlet-content'>" + "<p>Content</p>" + "<img src=" + img + "><p>Rating:" + rating + "</p></div></div>");
 
 
-$(document).ready(function(){
-    $( ".column" ).sortable({
-      connectWith: ".column",
-      handle: ".portlet-header",
-      cancel: ".portlet-toggle",
-      placeholder: "portlet-placeholder ui-corner-all"
+}
+
+//--loginUser API call to log user into site
+function loginUser(username, password) {
+    $('p.error').empty();
+    let q_string = {
+        'username': username,
+        'password': password
+    };
+    $.ajax({type: "POST", url: "/login", data: q_string, dataType: 'json'}).done(function(result) { //this waits for the ajax to return with a succesful promise object
+        //display main page here after logging in
+        console.log("successful login");
+        mainPage(result);
+    }).fail(function(jqXHR, error) {
+        //User login was unsuccessful, due to pw/username combination was wrong
+        $('p.login_error').text("We're sorry, that un/pw combination was incorrect.");
     });
-    $( ".portlet" )
-      .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
-      .find( ".portlet-header" )
-        .addClass( "ui-widget-header ui-corner-all" )
-        .prepend( "<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
+}
 
-    $( ".portlet-toggle" ).on( "click", function() {
-      var icon = $( this );
-      icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
-      icon.closest( ".portlet" ).find( ".portlet-content" ).toggle();
+//--newUser API to create a new user from login/signup main page
+function newUser(username, password) {
+    $('p.error').empty();
+    var q_string = {
+        'username': username,
+        'password': password
+    };
+    $.ajax({type: "POST", url: "/users/create", data: q_string, dataType: 'json'}).done(function(result) {
+        if (result.username) {
+            mainPage(result);
+        } else {
+            $('p.newuser_error').text("Sorry, that username exists already, try another username");
+        }
+    }).fail(function(jqXHR, error) { //this waits for the ajax to return with an error promise object
+        $('p.newuser_error').text("We're sorry, there was a system error, try again.");
+    });
+}
+
+//--newUser API to create a new user from login/signup main page
+function searchAPI(search_params) {
+    $('p.error').empty();
+    let search_string = search_params;
+    const app_key = '4cc7572d414ad3533abecb16976baa15';
+    const app_id = 'ada49da9';
+    $.ajax({
+        type: "GET",
+        url: "http://api.yummly.com/v1/api/recipes?_app_id=" + app_id + "&_app_key=" + app_key + "&q=" + search_params,
+        dataType: 'jsonp'
+    }).done(function(result) {
+        console.log(result);
+        result.matches.forEach(dispSearch);
+    }).fail(function(jqXHR, error) { //this waits for the ajax to return with an error promise object
+        $('p.newuser_error').text("We're sorry, there was a system error, try again.");
+    });
+}
+
+//Displays the main layout page
+function mainPage(result) {
+    $('section.login_transparency').css('display', 'none');
+
+    $('section.container').css('display', 'block');
+    $('section.side_search').css('display', 'block');
+    $('section.side_faves').css('display', 'block');
+}
+
+//Document Section
+$(document).ready(function() {
+    //display the login block
+    $('section.login_transparency').css('display', 'block');
+
+    //hide the search and display blocks until logged in
+    $('section.container').css('display', 'none');
+    $('section.side_search').css('display', 'none');
+    $('section.side_faves').css('display', 'none');
+
+    //Handle login event, call API to login user
+    $('#login').submit(function(event) {
+        event.preventDefault();
+        var username = $('input#username').val();
+        var password = $('input#password').val();
+        loginUser(username, password);
     });
 
-    $(".search-slide").click(function(){
-      $("#panel").slideToggle("slow");
-      $("i").toggleClass("fa-plus fa-minus");
-    });
-    $(".faves-slide").click(function(){
-      $("#panel_faves").slideToggle("slow");
-      $("i").toggleClass("fa-plus fa-minus");
+    //Handle new user registration event, call API to add user
+    $('#new_user').submit(function(event) {
+        event.preventDefault();
+        var username = $('input#new_username').val();
+        var password = $('input#new_password').val();
+        if (!username && password) {
+            $('p.error').text("Must enter a username/password for a new user signup.");
+        } else {
+            newUser(username, password);
+        }
     });
 
-  });
+    //Recipe/food search, search API from form
+    $('#foodsearch').submit(function(event) {
+        event.preventDefault();
+        var recipe_search = $('select#recipe_search').val();
+        var food_search = $('input#food_search').val();
+        console.log(recipe_search, food_search);
+        if(recipe_search && food_search) {
+            console.log("selected two");
+            $('p.search_error').text("Please only choose recipes OR food search");
+        } else if (recipe_search || food_search) {
+            //Search Yummly, can't have both defined, but API call will determine which one to use
+            searchAPI(recipe_search, food_search);
+        } else {
+           //didn't select anything!
+            $('p.search_error').text("Please choose from one of the above.");
+        }
+
+    });
+
+    $(".column").sortable({connectWith: ".column, .column_results",
+    start: function (event, ui) {
+      console.log("picked up");
+            ui.item.toggleClass("highlight");
+   },
+   stop: function (event, ui) {
+            ui.item.toggleClass("highlight");
+   }, handle: ".portlet-header", cancel: ".portlet-toggle", placeholder: "portlet-placeholder ui-corner-all"});
+
+    $(".column_results").sortable({connectWith: ".column, .column_results",
+    start: function (event, ui) {
+      console.log("picked up");
+            ui.item.toggleClass("highlight");
+   },
+   stop: function (event, ui) {
+            ui.item.toggleClass("highlight");
+   }, handle: ".portlet-header", cancel: ".portlet-toggle", placeholder: "portlet-placeholder ui-corner-all"});
+
+    $(".portlet").addClass("ui-widget ui-widget-content ui-helper-clearfix ui-corner-all").find(".portlet-header").addClass("ui-widget-header ui-corner-all").prepend("<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
+
+    $(".portlet-toggle").on("click", function() {
+        var icon = $(this);
+        icon.toggleClass("ui-icon-minusthick ui-icon-plusthick");
+        icon.closest(".portlet").find(".portlet-content").toggle();
+    });
+
+    $(".search-slide").click(function() {
+        $("#panel").slideToggle("slow");
+        $("i").toggleClass("fa-plus fa-minus");
+    });
+    $(".faves-slide").click(function() {
+        $("#panel_faves").slideToggle("slow");
+        $("i").toggleClass("fa-plus fa-minus");
+    });
+
+});
