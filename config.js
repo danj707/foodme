a73fac6cd58aeae17a33778c52cd9901620004a3
Dan Jenner <danjenner@gmail.com>
2016-11-17 16:26:36 -0600

    Changed config file

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                       'mongodb://localhost/foodme' :
-                       'mongodb://admin:production@ds153667.mlab.com:53667/foodme');
+                       'mongodb://admin:production@ds153667.mlab.com:53667/foodme' :
+                       'mongodb://localhost/foodme');
 exports.PORT = process.env.PORT || 8080;
