90e355145a6601301a33a615a718ccb93fac8b12
Dan Jenner <danjenner@gmail.com>
2016-11-17 16:17:04 -0600

    Updating Heroku config

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
                        'mongodb://localhost/foodme' :
-                       'mongodb://admin:foodme@ds153667.mlab.com:53667/foodme');
+                       'mongodb://admin:production@ds153667.mlab.com:53667/foodme');
 exports.PORT = process.env.PORT || 8080;
